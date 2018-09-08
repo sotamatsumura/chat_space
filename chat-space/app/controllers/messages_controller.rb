7bd5f3c749d1c3c93e17c89b97b01b9a0bdf2c66
sotamatsumura <sota.matsumura0228@gmail.com>
2018-09-08 14:46:10 +0900

    Messages error clear

--- chat-space/app/controllers/messages_controller.rb
+++ chat-space/app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
-    @message = @group.messages.includes(:user)
+    @messages = @group.messages.includes(:user)
   end
 
   def create
