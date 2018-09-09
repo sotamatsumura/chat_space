c1e4a3282590bebe0fe81f6d5baac19fb83c89c8
sotamatsumura <sota.matsumura0228@gmail.com>
2018-09-09 13:06:32 +0900

    Js ajax edit commit

--- chat-space/app/assets/javascripts/comment.js
+++ chat-space/app/assets/javascripts/comment.js
@@ -1,10 +1,38 @@
 $(function(){
+  function buildHTML(message){
+    var html = `<p>
+                  <strong>
+                    <a href=/groups/${message.groups_id}/messages></a>
+                    ：
+                  </strong>
+                  ${message.content}
+                </p>`
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log("success");
+    console.log(this);
     var formData = new FormData(this);
-    for(item of formData){
-      console.log(item);
+    // for(item of formData){
+    //   console.log(item);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
     }
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.content__row-mini-lower').append(html)
+      $('.content__footer-text-message').val('')
+      var datas = $("content__row-left" + data);
+      $(window).scrollTop(data.offset().top);
+    })
+    .fail(function(){
+      alert('error');
+    })
   });
 });
---
+++ chat-space/app/views/messages/create.json.jbuilder
@@ -0,0 +1,3 @@
+json.content  @message.content
+json.image  @message.image
+json.group_id  @message.group_id
