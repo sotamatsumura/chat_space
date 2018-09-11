4ff37c58a64b290f355957a0db075f530aabb172
sotamatsumura <sota.matsumura0228@gmail.com>
2018-09-11 15:08:53 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	chat-space/app/models/group.rb
    #	chat-space/app/views/messages/index.html.haml

--- chat-space/app/models/group.rb
+++ chat-space/app/models/group.rb
@@ -4,11 +4,12 @@ class Group < ApplicationRecord
   validates :name, presence: true
   has_many :messages
 
-  def show_last_message
+ def show_last_message
     if (last_message = messages.last).present?
-      last_message.conetent? ? last_message.content : '画像が投稿されています'
-      else
-        'まだメッセージはありません。'
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
     end
   end
 end
+
