1a97c6db9621c5cb8d51bf4fc2f17d764cccc11f
sotamatsumura <sota.matsumura0228@gmail.com>
2018-09-12 17:15:27 +0900

    delete_botton_add

--- chat-space/app/assets/javascripts/search.js
+++ chat-space/app/assets/javascripts/search.js
@@ -1,10 +1,18 @@
 $(function() {
-  function buildHTML(user){
+  function AddBuildHTML(user){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">
                     ${ user.name }
                   </p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
+                </div>`
+    return html;
+  }
+  function RemoveBuildHTML(user){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                <input name='group[user_ids][]' type='hidden' value="${ user.id }">
+                <p class='chat-group-user__name'>${ user.name }</p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>`
     return html;
   }
@@ -22,7 +30,7 @@ $(function() {
           datas = data
         });
       }
-      var html = buildHTML(datas);
+      var html = AddBuildHTML(datas);
       $('#user-search-result').append(html)
     })
     .fail(function(){
@@ -30,6 +38,8 @@ $(function() {
     })
   });
   $(document).on("click", ".user-search-add", function () {
-      $(".chat-group-user").empty();
+    $(".chat-group-user").empty();
+    var html = RemoveBuildHTML(datas);
+    $('#chat-group-users').append(html)
   });
 });
