b46cc3bb9950d6452fa73a19ae8ca525a5605d8c
sotamatsumura <sota.matsumura0228@gmail.com>
2018-09-12 14:26:47 +0900

    Routing commit

--- chat-space/config/routes.rb
+++ chat-space/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:inex, :edit, :update]
   resources :groups, only: [:new, :edit, :update, :create,] do
     resources :messages, only: [:index, :create]
   end
