b45a1beabd179baff8374ba8937df713f6f7b80c
sotamatsumura <sota.matsumura0228@gmail.com>
2018-09-12 15:47:34 +0900

    Controller edit html

--- chat-space/app/controllers/users_controller.rb
+++ chat-space/app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def index
     @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
     respond_to do |format|
+      format.html
       format.json
     end
   end
