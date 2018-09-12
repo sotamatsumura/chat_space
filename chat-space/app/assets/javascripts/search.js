da5b81847ef01c96ec0b2a9e796f26067842c260
sotamatsumura <sota.matsumura0228@gmail.com>
2018-09-12 16:03:14 +0900

    Ajax commit

--- chat-space/app/assets/javascripts/search.js
+++ chat-space/app/assets/javascripts/search.js
@@ -1,29 +1,28 @@
 $(function() {
-//   function buildHTML(user){
-//     var html = `<div class="chat-group-user clearfix">
-//                   <p class="chat-group-user__name">
-//                     ${ user.name }
-//                   </p>
-//                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
-//                 </div>`
-//     return html;
-//   }
+  function buildHTML(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">
+                    ${ user.name }
+                  </p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
+                </div>`
+    return html;
+  }
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-//     $.ajax({
-//       type: 'GET',
-//       url: '/users',
-//       data: { keyword: input },
-//       dataType: 'json'
-//     })
-//     .done(function(data){
-//       console.log(data);
-//       // $("#user-search-field").off("keyup")
-//       // console.log(data.id);
-//         // var html = buildHTML(data);
-//         // $('.chat-group-user__name').append(html)
-//       // $('.chat-group-form__input').val('')
-//     })
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(data){
+      for (var i = 0; i < data.length; i++) {
+        datas = (data[i]);
+      }
+      var html = buildHTML(datas);
+      $('#chat-group-users').append(html)
+    })
 //     .fail(function(){
 //       alert('error');
 //     })
