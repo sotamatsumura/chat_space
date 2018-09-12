1e6678af27de87650d8c96f3dbf08029d286dccd
sotamatsumura <sota.matsumura0228@gmail.com>
2018-09-12 15:02:03 +0900

    Routing edit index

--- chat-space/config/routes.rb
+++ chat-space/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
-  resources :users, only: [:inex, :edit, :update]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :edit, :update, :create,] do
     resources :messages, only: [:index, :create]
   end
