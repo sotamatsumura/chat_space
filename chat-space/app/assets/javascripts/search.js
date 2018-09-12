ff4c2b542efcd73f7b81e7765010bf10dedf1e70
sotamatsumura <sota.matsumura0228@gmail.com>
2018-09-12 14:38:17 +0900

    json add

---
+++ chat-space/app/assets/javascripts/search.js
@@ -0,0 +1,31 @@
+$(function() {
+//   function buildHTML(user){
+//     var html = `<div class="chat-group-user clearfix">
+//                   <p class="chat-group-user__name">
+//                     ${ user.name }
+//                   </p>
+//                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
+//                 </div>`
+//     return html;
+//   }
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+//     $.ajax({
+//       type: 'GET',
+//       url: '/users',
+//       data: { keyword: input },
+//       dataType: 'json'
+//     })
+//     .done(function(data){
+//       console.log(data);
+//       // $("#user-search-field").off("keyup")
+//       // console.log(data.id);
+//         // var html = buildHTML(data);
+//         // $('.chat-group-user__name').append(html)
+//       // $('.chat-group-form__input').val('')
+//     })
+//     .fail(function(){
+//       alert('error');
+//     })
+  });
+});
