5856937d76ade2e78d5afd1292f8be16bd15bc12
sotamatsumura <sota.matsumura0228@gmail.com>
2018-09-13 16:02:01 +0900

    auto_fix commit

--- chat-space/app/assets/javascripts/message.js
+++ chat-space/app/assets/javascripts/message.js
@@ -1,13 +1,47 @@
 $(function() {
+  function buildHTML(message) {
+    var insertImage = '';
+    if (message.image.url) {
+      insertImage = `<img src="${message.image.url}">`;
+    }
+    var html = `
+              <div class="content-ajax">
+                <div class="content__row-left">
+                  <div class="content__row-left-text"">
+                  ${message.name}
+                  </div>
+                  <div class="content__row-left-days"">
+                  ${message.date}
+                  </div>
+                </div>
+                <div class="content__row-mini">
+                  <p class="content__row-mini-lower">
+                  ${message.content}
+                  </p>
+                  ${insertImage}
+                </div>
+              </div>
+               `;
+    return html
+  }
+
   setInterval(function() {
-  $.ajax({
-    url: location.href.json,
-  })
-  .done(function(data) {
-  })
-  .fail(function(data) {
-  });
-} else {
-  clearInterval(interval);
- } , 5000 );
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      dataType: 'json'
+    })
+    .done(function(data) {
+      var id = $('.content-ajax').data('messageId');
+      var insertHTML = '';
+      data.messages.forEach(function(message) {
+        if (message.id > id)
+          insertHTML += buildHTML(message);
+      });
+      $('.content__row').prepend(insertHTML);
+    })
+    .fail(function(){
+      alert('error');
+    });
+  }, 5000);
 });
